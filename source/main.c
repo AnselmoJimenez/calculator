b6e841f3dfc9f4f5b66aa15b00491509a6385940
Anselmo Jimenez <anselmojimenez0@gmail.com>
2025-04-25 19:39:32 -0400

    Created .gitignore

---
+++ source/main.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+enum tokens { NONE, PARENTHESES, OPERAND, OPERATION, EXPONENT, FUNCTION};
+
+#define MAXTOKENLEN 50
+
+char token[MAXTOKENLEN] = { '\0' };
+enum tokens tokentype = 0;
+
+void expression(void);
+void term(void);
+void factor(void);
+void power(void);
+void atom(void);
+
+int gettoken(void);
+
+int main(void) {
+    while (gettoken() != EOF) {
+        printf("%s\n", token);
+        memset(token, '\0', MAXTOKENLEN);
+    }
+
+    return 0;
+}
+
+int getch(void);
+void ungetch(int c);
+
+// gettoken : Get the next token from the input
+int gettoken(void) {
+    char c = 0;
+    char *tp = token;
+
+    while (isspace(c = getch()) && c != '\n')
+        ;
+    
+    switch (c) {
+    case '(':
+    case ')':
+        *tp++ = c;
+        *tp = '\0';
+        return tokentype = PARENTHESES;
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+        *tp++ = c;
+        *tp = '\0';
+        return tokentype = OPERATION;
+    case '^':
+        *tp++ = c;
+        *tp = '\0';
+        return tokentype = EXPONENT;
+    default:
+        if (isdigit(c)) {   // Collect number token
+            for (*tp++ = c; isdigit(c = getch()); *tp++ = c)
+                ;
+            
+            if (c == '.') *tp++ = c;
+            else return tokentype = OPERAND;
+            
+            // collect decimal portion
+            if (isdigit(c = getch())) {
+                for (*tp++ = c; isdigit(c = getch()); *tp++ = c)
+                    ;
+                *tp = '\0';
+                ungetch(c);
+            }
+            else {
+                *tp++ = '0';
+                *tp = '\0';
+            }
+
+            return tokentype = OPERAND;
+        }
+        else if (isalnum(c)) {  // collect function
+            for (*tp++ = c; isalnum(c = getch()); *tp++ = c)
+                ;
+            *tp = '\0';
+            ungetch(c);
+            return tokentype = FUNCTION;
+        }
+        else {
+            tokentype = NONE;
+            return c;
+        }
+    }
+}
+
+
+#define MAXBUFFER  100
+
+static char cbuffer[MAXBUFFER] = { '\0' };
+static char *cbp = &cbuffer[0];
+
+int getch(void) {
+    return (cbp > cbuffer) ? *--cbp : getchar();
+}
+
+void ungetch(int c) {
+    if (cbuffer + MAXBUFFER - cbp <= 0) printf("cbuffer: Stack is full");
+    else *cbp++ = c;
+}
