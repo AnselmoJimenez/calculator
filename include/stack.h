c9e132718cc7136a84f1bb0f7e18b0600a523d0f
Anselmo Jimenez <anselmojimenez0@gmail.com>
2025-04-27 18:13:05 -0400

    implemented a stack

---
+++ include/stack.h
@@ -0,0 +1,8 @@
+// push : Push a character onto the stack
+void push(int);
+
+// pop : return the character at the top of the stack
+int pop(void);
+
+// printstack : print the current contents of the stack
+void printstack(void);
---
+++ source/stack.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define MAXSTACK 100
+
+static char cstack[MAXSTACK] = {'\0'};
+static char *cp = &cstack[0];
+
+// push : Push a character onto the stack
+void push(int c) {
+    if (cstack + MAXSTACK > cp) *cp++ = c; 
+}
+
+// pop : return the character at the top of the stack
+int pop(void) {
+    char tmp = *--cp;
+    *cp = '\0';
+    return tmp;
+}
+
+// printstack : print the current contents of the stack
+void printstack(void) {
+    printf("<");
+    for (int i = 0; i < MAXSTACK && cstack[i] != '\0'; i++)
+        printf("%c, ", cstack[i]);
+    printf("\b\b>\n");
+}
